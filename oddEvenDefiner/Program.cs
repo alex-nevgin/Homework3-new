8e40a13d2f56e443c48c7bd9500baa1892d5ade0
Alexander Nevgin <alexblackstar86@gmail.com>
2021-10-09 17:30:07 +0300

    Homework3-new started. Added oddEvenDefiner; started primeNumberDefiner

---
+++ oddEvenDefiner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace oddEvenDefiner
+{
+    /// <summary>
+    /// Программа, определяющая четность или нечетность введенного пользователем числа
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num; // вводимое число
+            
+            Console.WriteLine("Введите число: ");
+            num = int.Parse(Console.ReadLine());
+            
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Введенное число - четное");
+            }
+            else
+            {
+                Console.WriteLine("Введенное число - нечетное");
+            }
+        }
+    }
+}
---
+++ primeNumberDefiner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace primeNumberDefiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int rem = 0;
+            bool ifPrime;
+
+            Console.WriteLine("Введите число: ");
+            num = int.Parse(Console.ReadLine());
+
+            int i = num;
+
+            while (i <= i - 1)
+            {
+
+                
+                if (rem == 0)
+                {
+                    Console.WriteLine("Найден делитель. Число не является");
+                }
+            }
+        }
+    }
+}
