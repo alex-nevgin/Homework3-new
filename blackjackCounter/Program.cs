fb795b41a3980a1d81c24dfd689170c62162c783
Alexander Nevgin <alexblackstar86@gmail.com>
2021-10-11 18:28:28 +0300

    blackJackCounter added

--- blackjackCounter/Program.cs
+++ blackjackCounter/Program.cs
@@ -6,7 +6,79 @@ namespace blackjackCounter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("Введите число карт: ");
+            int handCards = int.Parse(Console.ReadLine());
+
+            int handValue = 0;
+
+            for (int i = 1; i <= handCards; i++)
+            {
+                Console.WriteLine("Введите номинал карты: ");
+                string cardValue = Console.ReadLine();
+                int Value;
+                switch (cardValue)
+                {
+                    case "2":
+                        Value = Convert.ToInt32(cardValue);
+                        handValue = Value;
+                        break;
+                    case "3":
+                        Value = Convert.ToInt32(cardValue);
+                        handValue = Value;
+                        break;
+                    case "4":
+                        Value = Convert.ToInt32(cardValue);
+                        handValue = Value;
+                        break;
+                    case "5":
+                        Value = Convert.ToInt32(cardValue);
+                        handValue = Value;
+                        break;
+                    case "6":
+                        Value = Convert.ToInt32(cardValue);
+                        handValue = Value;
+                        break;
+                    case "7":
+                        Value = Convert.ToInt32(cardValue);
+                        handValue = Value;
+                        break;
+                    case "8":
+                        Value = Convert.ToInt32(cardValue);
+                        handValue = Value;
+                        break;
+                    case "9":
+                        Value = Convert.ToInt32(cardValue);
+                        handValue = Value;
+                        break;
+                    case "10":
+                        Value = Convert.ToInt32(cardValue);
+                        handValue = Value;
+                        break;
+                    case "J":
+                        Value = 10;
+                        handValue = Value;
+                        break;
+                    case "Q":
+                        Value = 10;
+                        handValue = Value;
+                        break;
+                    case "K":
+                        Value = 10;
+                        handValue = Value;
+                        break;
+                    case "T":
+                        Value = 10;
+                        handValue = Value;
+                        break;
+                    default:
+                        Console.WriteLine("Допустимы номиналы 2-10, J, Q, K, T");
+                        break;
+                }
+
+                handValue = handValue+handValue;
+            }
+            Console.WriteLine($"Сумма карт руки: " + handValue);
         }
     }
 }
